7d508a9351ef084e158e6bdd5e27c0c958ad189a
Oluwafemi Bamisaye <femibami4u20@gmail.com>
2023-03-31 08:57:49 +0200

    Fix kotlin and compose compiler issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    id("com.github.ben-manes.versions") version "0.46.0"
-    id("nl.littlerobots.version-catalog-update") version "0.8.0"
 }
 
 //Workaround for "Expecting an expression" build error
