1d409e675dc38104b826b4028e001f32c420096e
Oluwafemi Bamisaye <femibami4u20@gmail.com>
2023-07-02 15:27:36 +0200

    Add java toolchain plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 java{
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 dependencies {
--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -12,5 +12,9 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
 rootProject.name = "build-logic"
 include(":convention")
