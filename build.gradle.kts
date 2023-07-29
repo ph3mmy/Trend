6a8bbcb037e9e5a9eb9812d94c6b5ea335e7b7ff
Oluwafemi Bamisaye <femibami4u20@gmail.com>
2023-07-30 00:26:06 +0200

    Clean up of Project build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,13 @@
-@file:Suppress("UnstableApiUsage")
-
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
-    dependencies {
-        classpath(libs.android.gradlePlugin)
-        classpath(libs.kotlin.gradlePlugin)
-    }
 }
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
 }
-
-//Workaround for "Expecting an expression" build error
-println("")
